8b6e8b2ac27da0ccf28a880feb24614a25e49e56
502296 <ali.kathem.edu@gmail.com>
2025-11-04 21:16:04 -0500

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
 
     const siteUrl = process.env.SITE_URL;
 
-    const gsUrl = process.env.GS_WEBAPP_URL || "https://script.google.com/macros/s/AKfycbzQ90OFgsod1WoHA_771Z8QhyoA1S70Pi3OMz0fmKFtexTpsU6CgQWn4Rb4YoW4aSlQ/exec";
+    const gsUrl = process.env.GS_WEBAPP_URL || "https://script.google.com/macros/s/AKfycbxK6Xnt_0ja0s9WFlijWNYinZ8RgD6j90uSgIX12yzn2R7Uc_EkhMMwCEQfDcN9iPEH/exec";
 
     // -----------------------------------------------------------------
 
